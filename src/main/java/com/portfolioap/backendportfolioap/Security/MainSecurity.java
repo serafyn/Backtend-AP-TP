058392c18f1287c7f41864dd4cf547baff7629ab
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-26 13:47:48 -0300

    up

--- src/main/java/com/portfolioap/backendportfolioap/Security/MainSecurity.java
+++ src/main/java/com/portfolioap/backendportfolioap/Security/MainSecurity.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
