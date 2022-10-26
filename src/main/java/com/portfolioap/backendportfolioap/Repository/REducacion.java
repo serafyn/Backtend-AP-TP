46bf411fad9bb9d640db41d56f53873dc34af9f2
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-26 15:15:36 -0300

    Entidad educacion y repositorio educacion

---
+++ src/main/java/com/portfolioap/backendportfolioap/Repository/REducacion.java
@@ -0,0 +1,12 @@
+package com.portfolioap.backendportfolioap.Repository;
+
+import com.portfolioap.backendportfolioap.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
