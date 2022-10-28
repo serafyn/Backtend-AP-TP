3c7174d4d3f3cdeb27c7940fef498cb68e8743e3
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-28 13:00:09 -0300

    Solucionando error de CORS

--- src/main/java/com/portfolioap/backendportfolioap/BackendportfolioapApplication.java
+++ src/main/java/com/portfolioap/backendportfolioap/BackendportfolioapApplication.java
@@ -2,12 +2,28 @@ package com.portfolioap.backendportfolioap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendportfolioapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendportfolioapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendportfolioapApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("")
+                        .allowedMethods("")
+                        .allowedHeaders("");
+            }
+        };
+
+    }
 }
