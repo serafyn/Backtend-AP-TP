e114f3699a2f92dc240d7e96fb0031581abf8bce
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-30 15:36:49 -0300

    up

--- src/main/java/com/portfolioap/backendportfolioap/BackendportfolioapApplication.java
+++ src/main/java/com/portfolioap/backendportfolioap/BackendportfolioapApplication.java
@@ -18,10 +18,10 @@ public class BackendportfolioapApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
-                        .allowedOrigins("")
-                        .allowedMethods("")
-                        .allowedHeaders("");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
 
